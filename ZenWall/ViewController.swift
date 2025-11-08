d8dfea2265f7227d501c3712e039de1cd3f133d0
ceyhunml <ceyhunmmli@gmail.com>
2025-11-08 11:07:00 +0400

    Start

---
+++ ZenWall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZenWall
+//
+//  Created by Ceyhun Məmmədli on 08.11.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
