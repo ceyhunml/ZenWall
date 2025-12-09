42f6a80144e8a5717b898e1556ef00474210db6d
ceyhunml <ceyhunmmli@gmail.com>
2025-12-09 12:11:25 +0400

    Add zenLogo asset and update launch screen
    Added zenLogo image to asset catalog and updated AppIcon configuration to use it. Modified LaunchScreen.storyboard to display zenLogo centered with a new background color. Minor update to CustomTabBar.swift for formatting.

--- ZenWall/Scene/CustomTabBar.swift
+++ ZenWall/Scene/CustomTabBar.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 
 final class CustomTabBar: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTabBarAppearance()
